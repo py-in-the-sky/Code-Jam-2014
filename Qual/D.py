79162f8e2b8c8feb29699c41eddfd1ea26d2a321
Ryan Wise <ryanmwise1@gmail.com>
2014-04-17 17:30:33 -0700

    D.py simulates, or corresponds to, a game
    changed D.py's code to more explicitly correspond to how the game would
    be played by people so that the code is easier to reason about.

--- Qual/D.py
+++ Qual/D.py
@@ -4,44 +4,57 @@ all blocks have different masses
 
 Ken's strategy: choose smallest mass larger than what Naomi announces; if he
     doesn't have a larger mass, then play smallest mass
-Naomi's deceitful strategy: announce a mass between Ken's two largest and then
-    actually play her smallest mass; after all her masses are larger than Ken's,
-    then she no longer has to be deceitful
-Naomi's war strategy: play from smallest to largest, announcing honestly
+See the functions deceitful and honest for Naomi's strategies
 """
 # from itertools import takewhile
 
-def deceitful(Naomi, Ken, naomi=0):
-    # Naomi = tuple(list(reversed(Naomi)))
-    # if not Naomi:
-    #     return naomi
+def deceitful(Naomi, Ken):
+    ## Naomi knows the masses of Ken's blocks
+    ## strategy: Naomi sacrifices her smallest blocks against Ken's largest blocks
+    Naomi.reverse()  # reversed for efficient removal of elements
+    ## Naomi now listed from largest to smallest
+    naomi_score = 0
     while Naomi:
-        while Naomi and Naomi[0]<Ken[0]:
-            Naomi = Naomi[1:]
-            Ken = Ken[:-1]
-        while Naomi and Naomi[0]>Ken[0]:
-            naomi += 1
-            Naomi = Naomi[1:]
-            Ken = Ken[1:]
-    # return deceitful(Naomi, Ken, naomi)
-    return naomi
+        ## each loop represents one round of the game for Naomi (i.e., decide
+        ## which block to play and play it, simulating Ken's response)
+        ## keep playing until no more blocks left (i.e., Naomi is an empty tuple)
+        n = Naomi.pop()
+        if n < Ken[0]:  # Naomi's smallest block is smaller than Ken's smallest
+            ## Naomi says its mass is between Ken's two largest blocks
+            Ken.pop()  # Ken plays his largest block and collects a point
+        elif n > Ken[0]:  # Naomi's smallest block is larger than Ken's smallest
+            ## this branch doesn't exactly simulate the game; rather, we know
+            ## that eventually in the game, Ken[0] will be beat by one of Naomi's
+            ## blocks since they have the same number of blocks and Ken[0] is
+            ## smaller than all of Naomi's
+            Ken.pop(0)  # so we get rid of Ken's smallest block
+            naomi_score += 1  # and Naomi (eventually) collects a point against that block
+
+    return naomi_score
 
 def honest(Naomi, Ken):
-    ken = 0
-    i = -1
+    ## strategy: just announce honestly
+    ken_score = 0  # Ken's points
+    i = -1  # index of the largest block played by Ken so far
     for n in Naomi:
+        ## for each block in Naomi's list, find the smallest of Ken's blocks
+        ## that haven't been played and give Ken a point, simulating him playing
+        ## that smallest block (loop invariant: i is the index of the largest
+        ## block played by Ken so far)
+        ## otherwise break out of the loop because all of Naomi's blocks are
+        ## larger than Ken's (i.e., Ken can't earn any more points)
         i = next((k+i+1 for k,e in enumerate(Ken[i+1:]) if e>n), None)
         if i is not None:
-            ken += 1
+            ken_score += 1
         else:
             break
-    return len(Naomi) - ken
+    return len(Naomi) - ken_score
 
 def solve(Naomi, Ken):
-    Naomi = tuple(sorted(Naomi))  # smallest to largest
-    Ken = tuple(sorted(Ken))
-    y = deceitful(Naomi, Ken)
+    Naomi = sorted(Naomi)  # smallest to largest
+    Ken = sorted(Ken)
     z = honest(Naomi, Ken)
+    y = deceitful(Naomi, Ken)  # deceitful mutates lists, so it comes after honest
     return (y, z)
 
 def main(fin, fout=None):
@@ -71,17 +84,17 @@ if __name__ == '__main__':
     problem = 'D'  # 'A', 'B', 'C', or 'D'
 
     ## TESTING ON PRACTICE INPUT
-    # with open(problem+'-practice.in', 'r') as fin:
-    #     main(fin)
+    with open(problem+'-practice.in', 'r') as fin:
+        main(fin)
 
     # # SMALL SUBMISSION
     # with open(problem+'-small-practice.in', 'r') as fin:
-    #     with open(problem+'-small4.out', 'w') as fout:
+    #     with open(problem+'-small.out', 'w') as fout:
     #         main(fin, fout)
 
     ## LARGE SUBMISSION
-    with open(problem+'-large-practice.in', 'r') as fin:
-        with open(problem+'-large2.out', 'w') as fout:
-            main(fin, fout)
+    # with open(problem+'-large-practice.in', 'r') as fin:
+    #     with open(problem+'-large.out', 'w') as fout:
+    #         main(fin, fout)
 
     print 'done!'
